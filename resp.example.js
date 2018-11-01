fa7fc39b685445e5c969a914d02604a080073bb1
jankern <janpkern@googlemail.com>
2018-11-01 21:20:05 +0100

    response examples changed

--- resp.example.js
+++ resp.example.js
@@ -1,14 +1,12 @@
-let response = {
-    "emailId": "rajan@calysto.in",
+let response_confirm = {
+    "emailId": "name@example.com",
     "token": "8391374e360b47b255982fd44a5830b1011f877c0129dfa2bae1903e91c85635",
     "tenantId": "incalr",
     "status": 2,
     "success": true
 }
 
-
-
-let response = {
+let response_register = {
     "success": false,
     "error": "tokenInvalid",
     "reqid": "<reqid>",
@@ -17,6 +15,3 @@ let response = {
         "error": "<error>"
     }]
 }
-
-https://tenny-dev.oxseco.net/register/
-janpkern@gmail.com,3146,84172b16c1871748488843c92b1ffc22e2675c47e866bc464df2895d9990bade
